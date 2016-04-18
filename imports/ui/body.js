940b8876d46c87f7965d6c7a8f38c9835fedbe25
Fredrik Teschke <f@ftes.de>
2016-04-18 17:11:12 +0200

    finish tutorial

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -16,6 +16,7 @@ import './task.js'
 
 Template.body.onCreated(function bodyOnCreated() {
     this.state = new ReactiveDict();
+    Meteor.subscribe('tasks');
 });
 
 Template.body.helpers({
@@ -58,13 +59,7 @@ Template.body.events({
         const target = event.target;
         const text = target.text.value;
 
-        // Insert a task into the collection
-        Tasks.insert({
-            text
-            , createdAt: new Date()
-                , owner: Meteor.userId()
-                , username: Meteor.user().username
-        });
+        Meteor.call('tasks.insert', text);
 
         // Clear form
         target.text.value = '';
